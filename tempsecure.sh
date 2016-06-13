fff74a69e5d7b0231dad498a9fbfafda1f02f28f
mjs2180 <mark@whitejade.net>
2016-06-13 22:11:15 +0800

    Update tempsecure.sh

--- tempsecure.sh
+++ tempsecure.sh
@@ -43,7 +43,7 @@ ENDL
 }
 
 # Create encrypted drive based on local ephemeral storage.
-# Key is randomly generated and stored on ramdisk temporaily, once encryption setup process has compelted it is deleted immediately
+# Key is randomly generated and stored on ramdisk temporaily, once encryption setup process has completed it is deleted immediately
 # LVM is used to stripe together multiple volumes into a single encrypted volume.
 # Input: No input
 encdrives () {
